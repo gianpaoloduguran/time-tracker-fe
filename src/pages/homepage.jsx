e4eb51d3e0fe725dfc9bbd2cf61b31c0c381b09c
gianpaoloduguran <paoloduguran@gmail.com>
2025-03-12 02:12:53 +0800

    feat: added homepage.

---
+++ src/pages/homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default function Homepage() {
+    return (
+
+        <div>
+            <ToastContainer />
+            homepage
+        </div>
+    )
+}
